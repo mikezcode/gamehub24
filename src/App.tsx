2c9841bb0983069cd6af768baaedb5f641a5dc0b
Tamiru Hailu <tamirumolla24@gmail.com>
2024-10-07 18:24:40 +1300

    setup git and chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Button, useColorMode } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-
+  const {colorMode,toggleColorMode} = useColorMode()
   return (
     <>
-  
+     <Button size={'x'} onClick={toggleColorMode} p={3} fontSize={'xx-large'}>
+      Toggle {colorMode==='dark'? 'light': 'dark'}
+     </Button>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App.tsx'
 import './index.css'
+import theme from './theme/theme.ts'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider>
+      {/* <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
