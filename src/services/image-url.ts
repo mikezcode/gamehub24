97ac5a6ac5a414c8abaaf725aedcd89f2b9f8288
Tamiru Hailu <tamirumolla24@gmail.com>
2024-10-24 17:52:12 +1300

    handle games without an image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import emptyImage from '../assets/empty.jpg'
+
 const cropImage = (url: string) => {
-  if(!url) return '';
+  if(!url) return emptyImage;
   const index = url.indexOf("media/") + 'media/'.length;  // /media.length =6 
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
