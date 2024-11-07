30e0e8814b9c3e439df9cda100c651b62e1736cf
Tamiru Hailu <tamirumolla24@gmail.com>
2024-11-07 19:26:13 +1300

    build expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,34 @@
+import { Button, Text } from "@chakra-ui/react";
+import React, { ReactNode, useState } from "react";
+
+interface Props {
+  children:string;
+}
+
+const ExpandableText = ({ children}: Props) => {
+  const [expandable, setExpandable] = useState(false);
+  const limit = 300;
+  // if (!children) return null;
+  
+  if (children.length < limit) return <Text>{children}</Text>;
+  if(!children) return null
+  const summary = expandable ? children : children.substring(0, 300) + "...";
+
+  return (
+    <Text>
+      {summary}
+      <Button
+        bg={"gold"}
+        color={"black"}
+        onClick={() => setExpandable(!expandable)}
+        ml={2}
+        size={'xs'}
+        fontWeight={'bold'}
+      >
+        {expandable ? "Show Less" : "Read More"}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableText;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,19 +1,18 @@
-import { useParams } from "react-router-dom"
-import useGame from "../hook/useGame"
-import { Heading, Spinner, Text } from "@chakra-ui/react"
+import { Heading, Spinner } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
+import useGame from "../hook/useGame";
 
 const GameDetailPage = () => {
-
-   const {slug} = useParams()
-   const {data:game,isLoading,error} = useGame(slug!)  
-   if(isLoading) return  <Spinner/>
-   if(error) throw error;
-  return ( 
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
+  if (isLoading) return <Spinner />;  
+  if (error) throw error;  return (
     <>
       <Heading>{game?.name}</Heading>
-      <Text>{game?.description_raw}</Text>
+      <ExpandableText >{game?.description_raw!}</ExpandableText>
     </>
   );
-}
+};
 
-export default GameDetailPage
+export default GameDetailPage;
